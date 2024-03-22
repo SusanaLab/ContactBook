b6c891c38a8513df1b0b15efd20df16a824eb1cc
Susana Garcia <susanlevart@gmail.com>
2024-03-21 18:58:21 -0600

    styles added to detail contact

--- src/components/common/ContactDetail.jsx
+++ src/components/common/ContactDetail.jsx
@@ -5,29 +5,29 @@ const ContactDetail = ({ contacts }) => {
   const { uuid } = useParams();
   const contact = contacts.find((c) => c.login.uuid === uuid);
 
-  if (!contact) return <div>Contact not found</div>;
 
-  const { name, location, email, phone, picture } = contact;
+
+  const { name, location, email, phone, picture, dob } = contact;
   const { title, first, last } = name;
   const { street, city, state, country, postcode, timezone } = location;
-
+  const birthDate = new Date(dob.date).toLocaleDateString();
   return (
     <div className="contact_detail">
-      <div>
-        {" "}
+      <div className="contact_name">
         <img src={picture.large} alt="Contact" />
         <h1>
           {title} {first} {last}
+          <h4>
+            {city}, {state}
+          </h4>
         </h1>
       </div>
-<div className="contact_info"> 
-  <h1>Contact Info </h1>
-  <h4>Phone: {phone}</h4>
-      <h4>Email: {email}</h4>
-      <h4>
-        Location: {city}, {state}, {country}
-      </h4></div>
-     
+      <div className="contact_info">
+        <h1>Contact Info </h1>
+        <h4>Phone: {phone}</h4>
+        <h4>Email: {email}</h4>
+        <h4>Date of birth: {birthDate}</h4>
+      </div>
     </div>
   );
 };
--- src/components/pages/contactList/ContactList.jsx
+++ src/components/pages/contactList/ContactList.jsx
@@ -5,7 +5,7 @@ import Footer from "../../layout/footer/Footer";
 const ContactList = ({ contacts }) => {
   return (
     <div className="contactList" >
-      <h2>Contact List </h2>
+      <h2>Contact Book </h2>
       <ul>
         {contacts.map((contact) => (
           <li key={contact.login.uuid}>
